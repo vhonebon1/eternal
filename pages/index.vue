9af0dfdb66ab7a4832e85d44d21e8ceb7077d15b
Verity Honebon <vhonebon@gmail.com>
2020-11-22 19:41:39 +0000

    move fetch to photo

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,9 @@
 <script>
 import Overlay from './../components/Overlay'
 
-var AWS = require('aws-sdk')
-
 export default {
   components: {
     Overlay
-  },
-  computed: {
-    s3 () {
-      return new AWS.S3()
-    },
-    config () {
-      return {
-        accessKeyId: `${process.env.AWS_ACCESS_KEY_ID_VH}`, 
-        secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY_VH}`, 
-        region: `${process.env.AWS_REGION_VH}`
-      }
-    },
-    params () {
-      return {
-        Bucket: `${process.env.AWS_BUCKET_VH}`,
-        Delimiter: '/',
-      }
-    }
-  },
-  methods: {
-    buildUrl (key) {
-      return `https://${process.env.AWS_BUCKET_VH}.s3.${process.env.AWS_REGION_VH}.amazonaws.com/${key}`
-    }
-  },
-  mounted () {
-    AWS.config.update(this.config)
-
-    let that = this
-
-    this.s3.listObjects(this.params, function (err, data) {
-      if (err) throw err
-      const imagePaths = data.Contents.map(image => that.buildUrl(image.Key)).reverse()
-      that.$store.commit('add', imagePaths)
-    })
   }
 }
 </script>
--- pages/photos.vue
+++ pages/photos.vue
@@ -2,6 +2,8 @@
 import PhotoContainer from './../components/PhotoContainer'
 import Nav from './../components/Nav'
 
+var AWS = require('aws-sdk')
+
 export default {
   name: 'photos',
   components: {
@@ -14,7 +16,19 @@ export default {
     }
   },
   mounted () {
+
     window.addEventListener('scroll', this.handleScroll)
+
+    AWS.config.update(this.config)
+
+    let that = this
+
+    this.s3.listObjects(this.params, function (err, data) {
+      if (err) throw err
+      const imagePaths = data.Contents.map(image => that.buildUrl(image.Key)).reverse()
+      that.$store.commit('add', imagePaths)
+    })
+    
   },
   destroyed () {
     window.removeEventListener('scroll', this.handleScroll)
@@ -22,6 +36,27 @@ export default {
   methods: {
     handleScroll (event) {  
       this.showNav = true
+    },
+    buildUrl (key) {
+      return `https://${process.env.AWS_BUCKET_VH}.s3.${process.env.AWS_REGION_VH}.amazonaws.com/${key}`
+    }
+  },
+  computed: {
+    s3 () {
+      return new AWS.S3()
+    },
+    config () {
+      return {
+        accessKeyId: `${process.env.AWS_ACCESS_KEY_ID_VH}`, 
+        secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY_VH}`, 
+        region: `${process.env.AWS_REGION_VH}`
+      }
+    },
+    params () {
+      return {
+        Bucket: `${process.env.AWS_BUCKET_VH}`,
+        Delimiter: '/',
+      }
     }
   }
 }
